07bfd69df0f6248af02985f20eb1df4b5d12ea9f
杨志文 <yangzhiwen@jqzms.com>
2022-09-15 18:36:35 +0800

    添加基础文件

--- miniprogram/utils/request.ts
+++ miniprogram/utils/request.ts
@@ -1,9 +1,22 @@
 import Storage from "../utils/storage";
 import { debounce } from "../utils/util";
+import { baseUrl } from "../utils/config";
 
+const BASEURL = `${baseUrl}/gshld-platform/enterprise/enterpriseApplicationParameter/getEAppIdByWAppId`;
 // 网络错误的页面
 // "/no-wifi/disabled-serve"
 // "/no-wifi/index"
+
+// 默认
+const defaultParam={
+  cliVersion: "",
+  sign: "",
+  signType: "",
+  source: "",
+  timestamp: 0,
+  version: "",
+}
+
 let requestCount = 0;
 // 加载
 const loading = debounce(function () {
@@ -16,11 +29,34 @@ const loading = debounce(function () {
       wx.hideLoading();
     }
   }, 200);
+//
 
+const getEpid=async()=>{
+  const [res] :any= await wx.request({
+    method: "POST",
+    url:  BASEURL,
+    data: {
+      appId: Storage.getJqzAppId() || "",
+      param:Storage.getWXAppId(),
+     ...defaultParam
+    },
+  });
+    const { appId, appType, epid } = res?.data?.data;
+    if (appId && appType && epid) {
+      Storage.setJqzAppId(appId)
+      Storage.setEpid(epid)
+    }
+    return epid;
+}
+
+
+
+
+// 请求
 const request = async (
   url: any,
   args: any,
-  method = "POST",
+  method:any = "POST" ,
   isLoading = true
 ) => {
   console.log(url);
@@ -51,6 +87,35 @@ const request = async (
   if (requestCount) {
     loading();
   }
+  try{
+    const [error, res] :any= await wx.request({
+      header: {
+        appId: Storage.getJqzAppId() || "",
+        wxAppid: Storage.getWXAppId(),
+        token: Storage.getToken,
+        sessionKey: '',
+        refreshToken:'',
+        epid:  Storage.getEpid() || getEpid(),
+      },
+      method,
+      url,
+      data: {
+        appId:Storage.getJqzAppId() || "",
+        param: args,
+        ...defaultParam
+      },
+    });
+
+    console.log(error)
+    console.log(res)
+
+  }catch{
+  
+
+
+  }
+
+
 };
 
 export default request;
--- miniprogram/utils/storage.ts
+++ miniprogram/utils/storage.ts
@@ -1,15 +1,52 @@
-// 
+//
 
-const appId = wx.getAccountInfoSync().miniProgram.appId
+const appId = wx.getAccountInfoSync().miniProgram.appId;
 class Storage {
-    //   import storage from "@/common/utils/storage";
+  // appId
+  static getWXAppId() {
+    return appId;
+  }
+  // JqzAppId
+  static setJqzAppId(value: string) {
+    wx.setStorageSync("jqzAppid", value);
+  }
+  static getJqzAppId() {
+    return wx.getStorageSync("jqzAppid");
+  }
+  static removeJqzAppId() {
+    return wx.removeStorageSync("jqzAppid");
+  }
+  // mid
+  static setMid(value: string) {
+    wx.setStorageSync("mid", value);
+  }
+  static getMid() {
+    return wx.getStorageSync("mid");
+  }
+  static removeMid() {
+    return wx.removeStorageSync("mid");
+  }
 
-    //   storage.setJqzAppId()   storage.setMid()   storage.setEpid()  storage.setToken()
+  // token
+  static setToken(value: string) {
+    wx.setStorageSync("token", value);
+  }
+  static getToken() {
+    return wx.getStorageSync("token");
+  }
+  static removeToken() {
+    return wx.removeStorageSync("token");
+  }
 
-    //   storage.getJqzAppId()   storage.getMid()   storage.getEpid()  storage.getToken()
-    static getJqzAppId() {
-        return appId
-    }
+  // epid
+  static setEpid(value: string) {
+    wx.setStorageSync("epid", value);
+  }
+  static getEpid() {
+    return wx.getStorageSync("epid");
+  }
+  static removeEpid() {
+    return wx.removeStorageSync("epid");
+  }
 }
-
-export default Storage
+export default Storage;
