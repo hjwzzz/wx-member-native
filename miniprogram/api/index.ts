a6a1fb0308ba5c439774483bbb795210ebff596b
杨志文 <yangzhiwen@jqzms.com>
2022-10-09 15:15:15 +0800

    更新首页

---
+++ miniprogram/api/index.ts
@@ -0,0 +1,17 @@
+// // 首页
+// export const wxmemberIndex = async (data) => {
+//   const url = `${http}/emp-base/wxmeberFront/wxmember/index`;
+//   const res = await request(url, data);
+//   return res;
+// };
+
+import type { wxmeberFront, member } from '../typings/api';
+import { baseUrl } from '../utils/config';
+import request from '../utils/request';
+
+// 根据微信APPID获取EAPPID
+export const wxmemberIndex = async (data: unknown) => {
+  const url = `${baseUrl}/emp-base/wxmeberFront/wxmember/index`;
+  const res = await request(url, data);
+  return res;
+};
--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -3,13 +3,28 @@
 
 // import Storage from "../../utils/storage";
 import { staticUrl } from '../../utils/config';
-// import { getEAppIdByWAppId } from "../../api/server";
+import { wxmemberIndex } from '../../api/index';
 
 // const { staticUrl }=getApp().require('/utils/config.js')
 // const app = getApp<IAppOption>()
 
 Page({
   data: {
+    bannerList: [],
+    dataList: {
+      bannerList: [],
+      wmMainRspVo: {
+        param: {
+          topBgImageUrl: null,
+        },
+      },
+    },
+    ///
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    interval: 2000,
+    duration: 500,
     staticUrl,
     motto: 'Hello World',
     userInfo: {},
@@ -20,26 +35,28 @@ Page({
       wx.canIUse('open-data.type.userAvatarUrl') &&
       wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
   },
+  onLoad() {
+    this.getPageDate();
+  },
+  async getPageDate() {
+    const result = await wxmemberIndex('');
+    const bannerList = result.data?.bannerList || [];
+    this.setData({
+      bannerList: bannerList,
+      dataList: result.data,
+    });
+    console.log(result);
+
+    wx.setNavigationBarTitle({
+      // 设置顶部bar的标题
+      title: result.data.wmMainRspVo?.param?.title,
+    });
+  },
   // 事件处理函数
   bindViewTap() {
     wx.navigateTo({ url: '../logs/logs' });
   },
-  onLoad() {
-    // console.log('staticUrl',staticUrl)
-    // console.log('Storage1111111111',Storage.getJqzAppId())
-    // console.log('ddd',ddd)
-    if ('getUserProfile' in wx) {
-      this.setData({ canIUseGetUserProfile: true });
-    }
 
-    // const dddd= ()=>{
-    //   getEAppIdByWAppId(Storage.getWXAppId()).then((res:any)=>{
-    //        console.log('getEAppIdByWAppId',res)
-    //   })
-
-    // }
-    // dddd()
-  },
   getUserProfile() {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
