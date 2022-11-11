01aeb8149e5b15f323d7ad867f0b626ba235bd71
周大同 <zhoudatong@kintree.cn>
2022-11-11 16:20:31 +0800

    feat: 添加导购ID

--- src/pages/api/appointment-store.ts
+++ src/pages/api/appointment-store.ts
@@ -2,22 +2,16 @@ import { devBaseUrl } from '@/utils/config';
 import request from '@/utils/request';
 
 // 根据分销商ID获取对应导购列表
-export const getSalerByDistId = async (data: any) => {
-  const url = `${devBaseUrl}/emp-base/usr/member/info/Front/getSalerByDistId`;
-  const res = await request(url, data);
-  return res;
-};
+export const getSalerByDistId = (data: any) => request(
+  `${devBaseUrl}/usr/operationDistributorInfoFront/querySalerListByDistId`,
+  data
+);
 
 // 根据服务ID查询可预约门店信息
-export const getServiceStore = async (data: any) => {
-  const url = `${devBaseUrl}/usr/bookingServe/queryServiceStoreListFront`;
-  const res = await request(url, data);
-  return res;
-};
+export const getServiceStore = (data: any) => request(`${devBaseUrl}/usr/bookingServe/queryServiceStoreListFront`, data);
 
 // 根据分销商ID获取对应导购列表
-export const getServiceShoppersList = async (data: any) => {
-  const url = `${devBaseUrl}/usr/bookingServe/queryShoppersListByDistIdFront`;
-  const res = await request(url, data);
-  return res;
-};
+export const getServiceShoppersList = (data: any) => request(
+  `${devBaseUrl}/usr/bookingServe/queryShoppersListByDistIdFront`,
+  data
+);
